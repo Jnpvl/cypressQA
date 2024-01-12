8271057427c1defa7eb0c5363a9511e706741b26
juan Vargas <juanpivl@hotmail.com>
2024-01-12 13:53:02 -0600

    template

---
+++ cypress/e2e/otroproyecto/constants.ts
@@ -0,0 +1,6 @@
+//constantes globales que se usaran en mas de un file
+export const SELECTORS={
+   inputEmail:'#input-10',
+   inputPassword :'#input-13',
+   buttonIniciar : '.v-btn__content',
+}
---
+++ cypress/e2e/otroproyecto/test/auth/login.cy.ts
@@ -0,0 +1,38 @@
+import { SELECTORS } from '../../constants';
+
+
+describe('Login Tests', () => {
+  beforeEach(() => {
+    const url = Cypress.env('URL');
+    cy.visit(url);
+  });
+
+  it('Login sin datos', function () {
+    cy.get(SELECTORS.buttonIniciar).click();
+    cy.contains('.v-messages__message', 'El correo es obligatorio').should('be.visible');
+    //cy.xpath('/html/body/div[1]/div/div/div/div/div/div/div/div/div[1]/div[3]/form/div[2]/div/div[2]/div/div/div', 'La contraseña es obligatoria').should('be.visible');
+
+  });
+
+
+  it('Login Incorrecto', function () {
+    cy.get(SELECTORS.inputEmail).type('noexisto@yopmail.com');
+    cy.get(SELECTORS.inputPassword).type('noexisto');
+    cy.get(SELECTORS.buttonIniciar).click();
+    cy.contains('#swal2-html-container', 'Error en las credenciales').should('be.visible');
+  });
+
+  const email = Cypress.env('USER_EMAIL');
+  const password = Cypress.env('USER_PASSWORD');
+
+  it('Login correcto', function () {
+    cy.get(SELECTORS.inputEmail).type(email);
+    cy.get(SELECTORS.inputPassword).type(password);
+    cy.get(SELECTORS.buttonIniciar).click();
+    cy.contains('button', 'Aceptar').click();
+    cy.get('.text-h4').should('be.visible');
+  });
+});
+
+
+
